25c8d6ec2f63aa147b6df57d7b053297b9174eb1
scherrda <dahlia.scherr@gmail.com>
2013-10-16 21:39:31 +0200

    fix

--- spec/elevatorEngineSpec.js
+++ spec/elevatorEngineSpec.js
@@ -1,49 +1,199 @@
 describe("Code Story Elevator", function() {
-	
+
+//init state	
 	it("init state: doors are closed.", function() {
-		expect(elevator.doors).toEqual("CLOSE");
+		expect(elevator.state).toEqual("CLOSE");
 		expect(elevator.isOpen()).toEqual(false);
 	});
 
 	it("init state:The Elevator is stopped.", function() {
-		expect(elevator.move).toEqual("STOP");
+		expect(elevator.moving).toEqual("STOP");
 	});
 
 	it("init state of commands list: empty", function() {
 		expect(elevator.commands.length).toEqual(0);
 	});
+
+	it("first call to the next step", function() {
+		expect(nextStep()).toEqual("NOTHING");
+	});
+
 	
+//first command	
 	it("adding first command", function() {
 		elevator.addCommand("new command");
 		expect(elevator.commands.length).toEqual(1);
 		expect(elevator.commands[0]).toEqual("new command");
 	});
 	
-	it("first call to the next command", function() {
-		expect(nextStep()).toEqual("NOTHING");
+	it("treating first command", function() {
+	    reset();
+		elevator.addCommand({floor : 1, direction : "UP"});
+		expect(nextStep()).toEqual("UP");//executing
+		expect(elevator.moving).toEqual("UP");//new state
+        expect(elevator.floor).toEqual(1);//id
+		expect(nextStep()).toEqual("OPEN");//executing
+		expect(nextStep()).toEqual("CLOSE");//finishing executing
+		expect(elevator.hasCommand()).toEqual(false);
+		expect(nextStep()).toEqual("NOTHING");//no command
 	});
-	
+
+//two commands
+	it("adding 2 commands, first for up side, 2nd for downside", function() {
+	    reset();
+	    elevator.floor = 2;
+		elevator.addCommand({floor : 3, direction : "upside for actual state"});
+		expect(elevator.commands.length).toEqual(1);
+		elevator.addCommand({floor : 1, direction : "downside for actual state"});
+		expect(elevator.commands.length).toEqual(2);
+
+		expect(elevator.commands[0].floor).toEqual(1);
+		expect(elevator.commands[1].floor).toEqual(3);
+		nextStep();
+		expect(elevator.state).toEqual("UP");
+		expect(elevator.moving).toEqual("UP");
+		expect(elevator.curIndex).toEqual(1);
+		expect(elevator.floor).toEqual(3);
+
+	});
+	it("2 commands - treating first for up side", function() {
+	    reset();
+	    elevator.floor = 2;
+		elevator.addCommand({floor : 3, direction : "upside for actual state"});
+		expect(elevator.curIndex).toEqual(0);//before 2nd command
+		elevator.addCommand({floor : 1, direction : "downside for actual state"});
+        expect(elevator.curIndex).toEqual(1);//after adding 2nd command downside
+
+		expect(nextStep()).toEqual("UP");//executing upside command
+ 	    expect(nextStep()).toEqual("OPEN");//executing upside command
+	    expect(nextStep()).toEqual("CLOSE");//finishing executing upside command
+
+		expect(elevator.commands.length).toEqual(1);
+		expect(elevator.curIndex).toEqual(0);
+	    expect(nextStep()).toEqual("DOWN");//begining executing downside command
+		expect(elevator.moving).toEqual("DOWN");
+
+	});
+
+
+	it("treating first command", function() {
+	    reset();
+		elevator.addCommand({floor : 1, direction : "UP"});
+		expect(nextStep()).toEqual("UP");//executing
+		expect(elevator.moving).toEqual("UP");//new state
+        expect(elevator.floor).toEqual(1);//id
+		expect(nextStep()).toEqual("OPEN");//executing
+		expect(nextStep()).toEqual("CLOSE");//finishing executing
+		expect(elevator.hasCommand()).toEqual(false);
+		expect(nextStep()).toEqual("NOTHING");//no command
+	});
+
+
+	it("next step of CLOSE is UP if command for up floor", function() {
+		reset();
+    	elevator.addCommand({floor : 5, direction : "DOWN"});
+    	expect(nextStep()).toEqual("UP");
+	});
+	it("next step of CLOSE is DOWN if command for down floor", function() {
+	    reset();
+		elevator.floor = 4;
+    	elevator.addCommand({floor : 2, direction : "DOWN"});
+    	expect(nextStep()).toEqual("DOWN");
+	});
+
+	it("next step of CLOSE is OPEN if command at same floor", function() {
+		reset();
+    	elevator.addCommand({floor : 0, direction : "UP"});
+    	expect(nextStep()).toEqual("OPEN");
+	});
+
+	it("next step of OPEN is CLOSE if command for other floor", function() {
+		elevator.state = "OPEN";
+		elevator.floor = 1;
+    	elevator.addCommand({floor : 5, direction : "DOWN"});
+    	expect(nextStep()).toEqual("CLOSE");
+	});
+	it("next step of OPEN is NOTHING if command for same floor", function() {
+		elevator.floor = 1;
+		elevator.state = "OPEN";
+		elevator.addCommand({floor : 1, direction : "DOWN"});
+
+    	expect(nextStep()).toEqual("NOTHING");
+    	expect(nextStep()).toEqual("CLOSE");
+	});
+
+	it("next step of OPEN is CLOSE if no more command", function() {
+		elevator.state = "OPEN";
+		elevator.floor = 1;
+    	expect(nextStep()).toEqual("CLOSE");
+	});
+
+	it("next step of UP is OPEN if arrived at floor", function() {
+    	elevator.addCommand({floor : 2, direction : "DOWN"});
+		elevator.state = "UP";
+		elevator.floor = 1;
+        var output = nextStep();
+    	expect(output).toEqual("UP");
+    	elevator.floor = 2;
+    	expect(nextStep()).toEqual("OPEN");
+    	expect(nextStep()).toEqual("CLOSE");
+	});
+	it("next step of DOWN is OPEN after arrived at floor", function() {
+	    reset();
+		elevator.state = "DOWN";
+		elevator.moving = "DOWN";
+		elevator.floor = 1;
+    	elevator.addCommand({floor : 0, direction : "DOWN"});
+    	expect(nextStep()).toEqual("DOWN");
+    	elevator.floor = 0;
+    	expect(nextStep()).toEqual("OPEN");
+    	expect(nextStep()).toEqual("CLOSE");
+	});
+
+
 	it("reset after adding a command", function() {
 		
 		elevator.addCommand("new command");
 		
 		expect(reset()).toEqual("");
-		expect(elevator.doors).toEqual("CLOSE");
+		expect(elevator.state).toEqual("CLOSE");
+		expect(elevator.noCommand()).toEqual(true);
 		expect(elevator.commands.length).toEqual(0);
 	});
 
 	it("calling the elevator up to first floor", function() {
-				
+		reset();
 		expect(call(1, "UP")).toEqual("");
-		expect(elevator.doors).toEqual("CLOSE");
+
 		expect(elevator.commands[0].floor).toEqual(1);
 		expect(elevator.commands[0].direction).toEqual("UP");
-		expect(elevator.nextFloor()).toEqual(1);
+
+		expect(nextStep()).toEqual("UP");
+		expect(elevator.state).toEqual("UP");
+		expect(elevator.floor).toEqual(1);
 	});
 
-	
+	it("command call up to first floor is treated", function() {
+		reset();
+		expect(call(1, "UP")).toEqual("");
+
+        nextStep();
+		expect(elevator.state).toEqual("UP");
+		expect(elevator.floor).toEqual(1);
+
+		nextStep();
+		expect(elevator.state).toEqual("OPEN");
+		expect(elevator.floor).toEqual(1);
+		expect(elevator.moving).toEqual("UP");
+		expect(elevator.noCommand()).toEqual(true);
+//TODO to be continued : testing nextfloor
+
+	});
+
+
 	it("3 calls to the elevator up and down", function() {
 		reset();
+		elevator.floor = 0;//will treated 2 then 3 then 5 floor calls
 		expect(call(5, "UP")).toEqual("");
 		expect(call(3, "DOWN")).toEqual("");
 		expect(call(2, "UP")).toEqual("");
@@ -51,7 +201,7 @@ describe("Code Story Elevator", function() {
 		expect(elevator.commands.length).toEqual(3);
 		expect(elevator.commands[0].floor).toEqual(2);
 		expect(elevator.commands[2].floor).toEqual(5);
-		expect(elevator.nextFloor()).toEqual(2);
+		expect(elevator.nextCommand().floor).toEqual(2);
 	});
 	
 	it("going to the first floor", function() {
@@ -78,6 +228,37 @@ describe("Code Story Elevator", function() {
 		expect(elevator.commands[0].floor).toEqual(3);
 		expect(elevator.commands[0].direction).toEqual("DOWN");
 	});
-	
-	
+
+	//review this test
+	it("special test case", function() {
+		reset();
+		elevator.floor = 1;
+		elevator.state = "OPEN";
+
+        elevator.addCommand({ floor: '4', direction: 'UP' });
+        elevator.addCommand({ floor: '4', direction: 'DOWN' });
+		elevator.addCommand({ floor: '4', direction: 'DOWN' });
+		elevator.addCommand({ floor: '5', direction: 'UP' });
+
+		expect(elevator.commands.length).toEqual(4);
+		expect(nextStep()).toEqual("CLOSE");
+		expect(nextStep()).toEqual("UP");
+        expect(nextStep()).toEqual("UP");
+		expect(nextStep()).toEqual("UP");
+
+        expect(elevator.floor).toEqual(4);
+		expect(elevator.commands.length).toEqual(4);
+		expect(nextStep()).toEqual("OPEN");
+		expect(elevator.commands.length).toEqual(3);
+        expect(nextStep()).toEqual("NOTHING");
+        expect(elevator.commands.length).toEqual(2);
+		expect(nextStep()).toEqual("NOTHING");
+		expect(nextStep()).toEqual("CLOSE");
+
+		expect(nextStep()).toEqual("UP");
+		expect(elevator.commands.length).toEqual(1);
+		expect(nextStep()).toEqual("OPEN");
+		expect(nextStep()).toEqual("CLOSE");
+		expect(elevator.commands.length).toEqual(0);
+	});
 });
--- src/elevatorEngine.js
+++ src/elevatorEngine.js
@@ -15,16 +15,23 @@ var elevator = {
     state : "CLOSE",
     commands : [],
     curIndex : 0,
-    move : "STOP",
+    moving : "STOP",
 
     isOpen : function(){
         return this.state == "OPEN";
     },
 
+    noCommand : function(){
+        return this.commands.length == 0;
+    },
+    hasCommand : function(){
+        return this.commands.length > 0;
+    },
+
 
     addCommand : function(command){
        var insertAt = 0;
-       if(this.commands.length == 0) {
+       if(this.noCommand()) {
             this.commands.push(command);
         } else {
             insertAt = this.locationOf(command);
@@ -42,7 +49,6 @@ var elevator = {
     locationOf : function (command, start, end) {
         start = start || 0;
         end = end || this.commands.length;
-        debugger;
         var pivot = parseInt(start + (end - start) / 2);
 
         if(end-start <= 1){
@@ -62,20 +68,51 @@ var elevator = {
         } else{
             return this.locationOf(command, start, pivot);
         }
-    },  
+    }, 
 
-    changeState : function(state){
+   nextState : function(){
+        var nextState ;
+
+		var command = this.nextCommand();
+        var toGo = command ? command.floor : null ;
+        var move = (this.floor > toGo) ? "DOWN" : ( (this.floor < toGo) ? "UP" : "STOP" );
+
+        switch(this.state){
+            case "CLOSE" :
+                nextState = ((null == toGo) ? "CLOSE" : (move == "STOP" ? "OPEN": move ) ) ;
+                break;
+            case "OPEN" :
+                nextState = ((toGo > -1) && (toGo == this.floor) ? "OPEN": "CLOSE") ;
+                break;
+            case "UP" :
+                nextState = ((move == "STOP") && toGo > -1) ? "OPEN": move ;
+                break;
+            case "DOWN" :
+                nextState = ((move == "STOP") && toGo > -1 ) ? "OPEN": move ;
+                break;
+            default :
+                nextState = "CLOSE";
+                break;
+        }
+        return nextState;
+    },	
+
+    changeState : function(){
+        var state = this.nextState();
         switch(state){
             case "OPEN":
                 this.state = "OPEN";
+                this.removeFinishedCommand();
                 break;
             case "UP" :
                 this.floor++;
                 this.state = "UP";
+				this.moving = "UP";
                 break ;
             case "DOWN":
                 this.floor--;
                 this.state = "DOWN";
+				this.moving = "DOWN";
                 break;
             case "CLOSE":
                 this.state = "CLOSE";
@@ -83,13 +120,49 @@ var elevator = {
             default :
                 break;
         }
+        return this.state;
     },
+	
+	isFinished : function(command){
+		return (command.floor == this.floor) && this.isOpen();
+	},
+	
+	newIndex : function(){
+		if(this.curIndex > this.commands.length - 1){
+			this.curIndex = this.commands.length - 1;
+			this.moving = "DOWN";
+		}else if(this.curIndex == 0){
+		    this.moving = "UP";
+		}else if(this.moving == "DOWN"){
+			this.curIndex --;
+			console.log("updating curIndex ", this.curIndex);
+		}	
+	},
+	
+	nextCommand : function(){
+	    if(!this.hasCommand()){
+	        return null;
+	    }
+		var next = this.commands[this.curIndex];
+		if(this.isFinished(next)){
+		    this.removeFinishedCommand();
+		    next = this.nextCommand();
+		}
+		return next;		
+	},
 
+	removeFinishedCommand : function(){
+        this.commands.splice(this.curIndex, 1); //la commande a été traitée
+        if(this.hasCommand()){
+            this.newIndex();
+        }
+	},
 
+/*
     nextFloor : function(){
     console.log("NEXT FLOOR commands before ", this.commands);
-        if(this.commands.length == 0){
-            return ;
+        if(this.noCommand()){
+            return null;
         }
 
        console.log("curIndex " + this.curIndex);
@@ -100,18 +173,18 @@ var elevator = {
             this.commands.splice(this.curIndex, 1); //la commande a été traitée
 
             if(this.commands.length == 0){
-                this.move = "STOP";
+                this.moving = "STOP";
                 console.log("no more command");
                 return ; //no more commands;
             }
 
             if(this.curIndex > this.commands.length - 1){
                 this.curIndex = this.commands.length - 1;
-                this.move = "DOWN";
+                this.moving = "DOWN";
                 console.log("changing direction to DOWN " + this.curIndex);
-            }else if(this.curIndex == 0 && this.move == "DOWN"){
-                this.move = "UP";
-            } else if(this.move == "DOWN"){
+            }else if(this.curIndex == 0 && this.moving == "DOWN"){
+                this.moving = "UP";
+            } else if(this.moving == "DOWN"){
                 this.curIndex --;
                 console.log("updating curIndex ", this.curIndex);
             }
@@ -121,16 +194,17 @@ var elevator = {
             next = this.commands[this.curIndex];
         }
 
-        if(this.move == "STOP"){
+        if(this.moving == "STOP"){
             if(next.floor >= this. floor){
-                this.move = "UP";
+                this.moving = "UP";
             }else{
-                this.move = "DOWN";
+                this.moving = "DOWN";
             }
         }
         console.log("NEXT FLOOR commands After ", this.commands);
         return next.floor;
     },
+*/
 
 
     reset : function(){
@@ -138,33 +212,19 @@ var elevator = {
         this.state = "CLOSE";
         this.commands = [];
         this.curIndex = 0 ;
-        this.move = "STOP";
+        this.moving = "STOP";
     }
 }
 
 function nextStep(){
-        var nextStep = "NOTHING";
-
         console.log("Actual State : ", elevator.floor + "e " + elevator.state);
 
-        var toGo = elevator.nextFloor();
-        var move = (elevator.floor > toGo) ? "DOWN" : ( (elevator.floor < toGo) ? "UP" : "STOP");
+        var actualState = elevator.state;
+        var nextState = elevator.changeState();
 
-        if(elevator.isOpen()){
-            if( move == "STOP"){
-                nextStep = "NOTHING";
-            }else{
-                nextStep = "CLOSE";
-            }
-        }else if( move == "STOP"){
-                nextStep = "OPEN";
-        }else{
-                nextStep = move;
-        }
-
-        elevator.changeState(nextStep);
         console.log("New State : ", elevator.floor + "e " + elevator.state );
-        return nextStep;
+        return ((nextState == actualState) && ( (nextState == "CLOSE") || (nextState == "OPEN")))
+                ? "NOTHING" : nextState ;
 }
 
 function reset(){
